eafdc05cdd006897ff5895b50ed8bb92ca665d70
Pablo Contreras <pacg1991@gmail.com>
2021-07-03 17:05:50 -0500

    api added logout login upgrade

--- app/Http/Controllers/Api/V1/LoginController.php
+++ app/Http/Controllers/Api/V1/LoginController.php
@@ -12,16 +12,42 @@ class LoginController extends Controller
 {
     public function login(Request $req)
     {
-        $this->validateLogin($req);
+        $errors = [];
+
+        $user = isset($req->user) ? $req->user : null;
+        $password = isset($req->password) ? $req->password : null;
+        $device = $req->hasHeader('device') ? $req->header('device') : null;
+
+        if (empty($user)) {
+            $errors = [...$errors, 'No indicó un usuario'];
+        }
+        if (empty($password)) {
+            $errors = [...$errors, 'No indicó una contraseña'];
+        }
+        if (empty($device)) {
+            $errors = [...$errors, 'No indicó un dispositivo'];
+        }
+
+        if (!empty($errors)) {
+            return response()->json([
+                'errors' => $errors,
+            ], 422);
+        }
 
         $credentials = [
-            'password' => $req->password,
+            'password' => $password,
         ];
 
-        if (filter_var($req->user, FILTER_VALIDATE_EMAIL)) {
-            $credentials['email'] = $req->user;
+        $where = [];
+
+        if (filter_var($user, FILTER_VALIDATE_EMAIL)) {
+            $credentials['email'] = $user;
+            $where['type'] = 'email';
+            $where['user'] = $user;
         }else{
-            $credentials['nickname'] = $req->user;
+            $credentials['nickname'] = $user;
+            $where['type'] = 'nickname';
+            $where['user'] = $user;
         }
 
         if (!Auth::attempt($credentials)) {
@@ -29,18 +55,34 @@ class LoginController extends Controller
                 'errors' => ['Unauthorized']
             ], 401);
         }
+        
+        $queryResult = User::where($where['type'],$where['user'])->first();
+
+        /*
+            if(!$user->tokens()->where('name', $request->device_name)->first()) {
+                return $user->createToken($request->device_name)->plainTextToken;
+            }
+            return $user->plainTextToken($request->device_name);
+        */
 
         return response()->json([
-            'token' => $req->user()->createToken($req->device)->plainTextToken,
+            'token' => $req->user()->createToken($device)->plainTextToken,
+            'name' => $queryResult['name'],
+            'nickname' => $queryResult['nickname'],
+            'email' => $queryResult['email'],
+        ]);
+    }
+    
+    public function logout(Request $req)
+    {
+        Auth::user()->tokens()->delete();
+
+        // Auth::user()->tokens()->where('id', $id)->delete();
+
+        return response()->json([
+            'message' => ['Deslogueo exitoso'],
         ]);
     }
 
-    public function validateLogin(Request $req)
-    {
-        return $req->validate([
-            'user' => 'required',
-            'password' => 'required',
-            'device' => 'required'
-        ]);
-    }
+
 }
